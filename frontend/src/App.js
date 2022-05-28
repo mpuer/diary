af9ab5837baba15cd35add4c411d205e692fb24f
Matthew <mattic11@yahoo.com>
2022-05-27 23:35:27 -0400

    small edits

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react';
 
 function App() {
+  const [post, setPost] = useState([])
+
+
+  useEffect(() => {
+  fetch('http://127.0.0.1/get', {
+    'method':'GET',
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  })
+  .then(res => res.json())
+  .then(res => console.log(res))
+  .catch(err => console.log(err))
+  }, [])
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello World!</h1>
     </div>
   );
 }
